c1e7da5c6eb34edd770bab445b5daa474476c144
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2020-06-01 21:21:12 -0600

    act #4

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('usuarios','usuariosController');
