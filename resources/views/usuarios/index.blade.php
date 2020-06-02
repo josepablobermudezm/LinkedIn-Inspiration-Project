a348854771f1077cace8618f33581253957d21b4
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2020-06-01 21:43:14 -0600

    act #5

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -7,40 +7,5 @@
     </div>
   </div>
   </div>
-
-  <!--@if ($message = Session::get('success'))
-      <div class="alert alert-success">
-          <p>{{ $message }}</p>
-      </div>
-  @endif
-
-  <table class="table table-bordered">
-    <tr>
-      <th with="80px">No</th>
-      <th>Title</th>
-      <th>Body</th>
-      <th with="140px" class="text-center">
-        <a href="{{route('usuarios.create')}}" class="btn btn-success btn-sm">
-          <i class="glyphicon glyphicon-plus"></i>
-        </a>
-      </th>
-    </tr>
-    //<?php $no=1; ?>
-    @foreach ($usuario as $key => $value)
-      <tr>
-        <td>{{$no++}}</td>
-        <td>{{ $value->title }}</td>
-        <td>{{ $value->body }}</td>
-        <td>
-          <a class="btn btn-info btn-sm" href="{{route('usuarios.show',$value->id)}}">
-              <i class="glyphicon glyphicon-th-large"></i></a>
-          <a class="btn btn-primary btn-sm" href="{{route('usuarios.edit',$value->id)}}">
-              <i class="glyphicon glyphicon-pencil"></i></a>
-            {!! Form::open(['method' => 'DELETE','route' => ['usuarios.destroy', $value->id],'style'=>'display:inline']) !!}
-              <button type="submit" style="display: inline;" class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash"></i></button>
-            {!! Form::close() !!}
-        </td>
-      </tr>
-    @endforeach
-  </table>-->
+  
 @endsection
