4d5250a0c7f98ebdac8c7b9c477bd6dfecf3823c
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2020-06-07 13:57:48 -0600

    act #42

--- resources/views/Reportes.blade.php
+++ resources/views/Reportes.blade.php
@@ -16,7 +16,9 @@
                     <a href="{{ url('usuarios') }}"><button id="Button2" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">-</label></button></a>
                     <a href="{{ url('ofertas') }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">-</label></button></a>
                     <a href="{{ url('categorias') }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">-</label></button></a>
-                    <a href="{{ route('pdf', auth()->user()->id) }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">Curriculum</label></button></a>
+                    <?php if (auth()->user()->tipoUsuario == 'C') { ?>
+                        <a href="{{ route('pdf', auth()->user()->id) }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">Curriculum</label></button></a>
+                    <?php } ?>
                 </ul>
             </nav>
         </div>
