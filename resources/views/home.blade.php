9a2420e87c113481fea5089655e60dbe9149472e
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2020-06-06 21:19:30 -0600

    act #38

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,7 @@
                                 <a href="{{ url('usuarios') }}"><button id="Button2" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-user-circle"></i><br><label style="font-size: 20px; cursor: pointer;">Mi Perfil</label></button></a>
                                 <a href="{{ url('ofertas') }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="far fa-address-card"></i><br><label style="font-size: 20px; cursor: pointer;">Ofertas</label></button></a>
                                 <a href="{{ url('categorias') }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-id-card-alt"></i><br><label style="font-size: 20px; cursor: pointer;">Categorías</label></button></a>
+                                <a href="{{ url('reportes') }}"><button id="Button3" class="Button1" onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label style="font-size: 20px; cursor: pointer;">Reportes</label></button></a>
                             </ul>
                         </nav>
                     </div>
