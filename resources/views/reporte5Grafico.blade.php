aa1ab71051f4170cfa247e5e5740dfc3ee14bdc3
JordiRodriguezC <ycorrales120@gmail.com>
2020-06-07 22:55:20 -0600

    :boom:

--- resources/views/reporte5Grafico.blade.php
+++ resources/views/reporte5Grafico.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,14 +8,14 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="jquery-1.3.2.min.js" type="text/javascript"></script>
     <title>Reporte 5</title>
 </head>
 
 <body>
-
-    <a href="#" id="downloadPdf">Download Report Page as PDF</a>
+    @foreach ($empresas as $key => $value)
+    <label for="">{{ $value->name }}</label>
+    @endforeach
+    <a href="#" id="downloadPdf">Descargar Como PDF</a>
     <br /><br />
     <div id="reportPage">
         <div id="chartContainer" style="width: 80%;float: left;">
@@ -45,9 +44,10 @@
         var randomScalingFactor = function() {
             return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
         };
-
+       
         var data = {
-            labels: ["Empresas", "N. Vacantes"],
+            labels:
+            [' ALGO '],
             datasets: [{
                 label: 'Fuel',
                 backgroundColor: [
