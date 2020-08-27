188845499b7446339d92741545a48350faf18ad5
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-08-27 14:39:12 -0600

    firt

---
+++ database/migrations/2020_08_27_201356_create_curriculums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->increments('crID');
+            $table->unsignedBigInteger('crUsuario');
+            $table->foreign('crUsuario')->references('id')->on('users');
+            $table->string('crObservaciones', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+}
