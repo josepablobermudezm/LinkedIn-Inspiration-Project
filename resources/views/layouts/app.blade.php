2b55c250f30f44585f30e61a01b617a64cf4ef9e
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-10-01 21:56:26 -0600

    Calendario Implementado Visual
    Ya se ve el calendario full funcional, falta la lógica de implementación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,16 @@
     <title>@yield('PageTitle') - SIRHENA</title>
 
     <!-- Scripts -->
+    <script src='fullcalendar/main.js'></script>
+    <script src='moment.min.js'></script>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@5.3.0/main.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,6 +27,7 @@
 
     <!-- Styles -->
     <!-- Styles -->
+    <link href='fullcalendar/main.css' rel='stylesheet' />
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
