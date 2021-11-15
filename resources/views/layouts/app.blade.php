f80e2ca5e6e86430cfc274193710266a0f390430
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-11-14 22:28:22 -0600

    cambio en reporte 3 y versión de bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
 
 <body  style="background-color: #f2f6f6; background-size:cover; " >
--- resources/views/reporte3Curriculum.blade.php
+++ resources/views/reporte3Curriculum.blade.php
@@ -18,52 +18,6 @@
 </head>
 
 <body>
-    <!-- <div class="container">
-        <img style="float: left;" src="img/company.png">
-        <h2 style='position: relative; text-align: center;'>Curriculum</h2>
-        <h2 style='padding-top: 60px; color: #636b6f;'>Datos Personales</h2>
-        <table class="table table-bordered" style=" text-align:center; margin-left:auto; margin-right:auto;">
-            <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Correo</th>
-                <th scope="col">Telefono</th>
-                <th scope="col">Direccion</th>
-                <th scope="col">Cedula</th>
-                <th scope="col">Foto</th>
-            </tr>
-          
-        </table>
-        <h2 style="color: #636b6f;">Experiencias</h2>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Puesto</th>
-                    <th scope="col">Empresa</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Fecha Inicio</th>
-                    <th scope="col">Fecha Final</th>
-                </tr>
-            </thead>
-        
-        </table>
-        <h2 style="color: #636b6f;">Formaciones</h2>
-        <table class="table table-bordered">
-            <tr>
-                <th scope="col">Titulo</th>
-                <th scope="col">Especialidad</th>
-                <th scope="col">Institución</th>
-                <th scope="col">Fecha</th>
-            </tr>
-            @foreach ($formaciones as $key => $value)
-            <tr>
-                <td>{{ $value->foTitulo }}</td>
-                <td>{{ $value->foEspecialidad }}</td>
-                <td>{{ $value->foInstitucion }}</td>
-                <td>{{ $value->foFecha }}</td>
-            </tr>
-            @endforeach
-        </table>
-    </div> -->
     <div class="wrapper mt-lg-5">
         <div class="sidebar-wrapper">
             @foreach ($usuarios as $key => $value)
@@ -86,8 +40,8 @@
             </div>
             @endforeach
             <!--//contact-container-->
-            <div class="education-container container-block">
-                <h2 class="container-block-title">Education</h2>
+            <!-- <div class="education-container container-block">
+                <h2 class="container-block-title">Educación</h2>
                 @foreach ($formaciones as $key => $value)
                 <div class="item">
                     <h4 class="degree">{{ $value->foTitulo }}</h4>
@@ -97,36 +51,8 @@
                     <div class="time">{{ $value->foFecha }}</div>
                 </div>
                 @endforeach
-
-                <!--//item-->
-                <div class="item">
-                    <h4 class="degree">BSc in Applied Mathematics</h4>
-                    <h5 class="meta">Bristol University</h5>
-                    <div class="time">2012 - 2016</div>
-                </div>
-                <!--//item-->
-            </div>
+            </div> -->
             <!--//education-container-->
-
-            <div class="languages-container container-block">
-                <h2 class="container-block-title">Languages</h2>
-                <ul class="list-unstyled interests-list">
-                    <li>English <span class="lang-desc">(Native)</span></li>
-                    <li>French <span class="lang-desc">(Professional)</span></li>
-                    <li>Spanish <span class="lang-desc">(Professional)</span></li>
-                </ul>
-            </div>
-            <!--//interests-->
-
-            <div class="interests-container container-block">
-                <h2 class="container-block-title">Interests</h2>
-                <ul class="list-unstyled interests-list">
-                    <li>Climbing</li>
-                    <li>Snowboarding</li>
-                    <li>Cooking</li>
-                </ul>
-            </div>
-            <!--//interests-->
 
         </div>
         <!--//sidebar-wrapper-->
@@ -166,115 +92,30 @@
                 @endforeach
                 <!--//item-->
             </section>
-            <!--//section-->
-            <section class="section projects-section">
-                <h2 class="section-title"><span class="icon-holder"><i class="fas fa-archive"></i></span>Projects</h2>
-                <div class="intro">
-                    <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit
-                        amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.
-                    </p>
-                </div>
-                <!--//intro-->
-                <div class="item">
-                    <span class="project-title"><a
-                            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/"
-                            target="_blank">CoderPro</a></span> - <span class="project-tagline">A responsive website
-                        template designed to help developers launch their software projects. </span>
 
-                </div>
-                <!--//item-->
+            <section class="section experiences-section">
+                <h2 class="section-title"><span class="icon-holder"><i class="fas fa-briefcase"></i></span>Educación
+                </h2>
+                @foreach ($formaciones as $key => $value)
                 <div class="item">
-                    <span class="project-title"><a
-                            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/launch-bootstrap-5-template-for-saas-businesses/"
-                            target="_blank">Launch</a></span> - <span class="project-tagline">A responsive website
-                        template designed to help startups promote their products or services.</span>
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">{{ $value->foTitulo }}</h3>
+                        </div>
+                        <!--//upper-row-->
+                        <div class="company">Especialidad: {{ $value->foEspecialidad }}</div>
+                        <div class="company">Institución: {{ $value->foInstitucion }}</div>
+                        <div class="company">Fecha: {{ $value->foFecha }}</div>
+
+                    </div>
+                    <!-- //meta-->
+                  
+                    <!--//details -->
                 </div>
-                <!--//item-->
-                <div class="item">
-                    <span class="project-title"><a
-                            href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/devcard-bootstrap-5-vcard-portfolio-template-for-software-developers/"
-                            target="_blank">DevCard</a></span> - <span class="project-tagline">A portfolio website
-                        template designed for software developers.</span>
-                </div>
-                <!--//item-->
-                <div class="item">
-                    <span class="project-title"><a
-                            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/bootstrap-template-for-mobile-apps-nova-pro/"
-                            target="_blank">Nova Pro</a></span> - <span class="project-tagline">A responsive Bootstrap
-                        theme designed to help app developers promote their mobile apps</span>
-                </div>
-                <!--//item-->
-                <div class="item">
-                    <span class="project-title"><a
-                            href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/"
-                            target="_blank">DevStudio</a></span> -
-                    <span class="project-tagline">A responsive website template designed to help web
-                        developers/designers market their services. </span>
-                </div>
+                @endforeach
                 <!--//item-->
             </section>
             <!--//section-->
-
-            <section class="skills-section section">
-                <h2 class="section-title"><span class="icon-holder"><i class="fas fa-rocket"></i></span>Skills &amp;
-                    Proficiency</h2>
-                <div class="skillset">
-                    <div class="item">
-                        <h3 class="level-title">Python &amp; Django</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 99%"
-                                aria-valuenow="99" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-
-                    <div class="item">
-                        <h3 class="level-title">Javascript</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 98%"
-                                aria-valuenow="98" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-
-                    <div class="item">
-                        <h3 class="level-title">React &amp; Angular</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 98%"
-                                aria-valuenow="98" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-
-                    <div class="item">
-                        <h3 class="level-title">HTML5 &amp; CSS</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 95%"
-                                aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-
-                    <div class="item">
-                        <h3 class="level-title">Ruby on Rails</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 85%"
-                                aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-
-                    <div class="item">
-                        <h3 class="level-title">Sketch &amp; Photoshop</h3>
-                        <div class="progress level-bar">
-                            <div class="progress-bar theme-progress-bar" role="progressbar" style="width: 60%"
-                                aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <!--//item-->
-                </div>
-            </section>
-            <!--//skills-section-->
         </div>
         <!--//main-body-->
     </div>
