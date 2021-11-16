95af7dea7c75bf984bf51291a607ab615d151d9d
JocserAraya <80366138+JocserAraya@users.noreply.github.com>
2021-11-15 20:38:17 -0600

    Ajustes en curriculum2

--- resources/views/reporte2Curriculum.blade.php
+++ resources/views/reporte2Curriculum.blade.php
@@ -19,71 +19,7 @@
 </head>
 
 <body>
-    <!-- <div class="container">
-        <img style="float: left;" src="img/company.png">
-        <h2 style='position: relative; text-align: center;'>Curriculum</h2>
-        <h2 style='padding-top: 60px; color: #636b6f;'>Datos Personales</h2>
-        <table class="table table-bordered" style=" text-align:center; margin-left:auto; margin-right:auto;">
-            <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Correo</th>
-                <th scope="col">Telefono</th>
-                <th scope="col">Direccion</th>
-                <th scope="col">Cedula</th>
-                <th scope="col">Foto</th>
-            </tr>
-            @foreach ($usuarios as $key => $value)
-            <tr>
-                <td>{{ $value->name }}</td>
-                <td>{{ $value->email }}</td>
-                <td>{{ $value->phone }}</td>
-                <td>{{ $value->address }}</td>
-                <td>{{ $value->cedula }}</td>
-                <td><img class="imagen" style="width:50px; height:50px;"
-                        src="<?php //echo ('storage/images/' . $value->photo) 
-                                ?>"></td>
-            </tr>
-            @endforeach
-        </table>
-        <h2 style="color: #636b6f;">Experiencias</h2>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Puesto</th>
-                    <th scope="col">Empresa</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Fecha Inicio</th>
-                    <th scope="col">Fecha Final</th>
-                </tr>
-            </thead>
-            @foreach ($experiencias as $key => $value)
-            <tr>
-                <td>{{ $value->exPuesto }}</td>
-                <td>{{ $value->exEmpresa }}</td>
-                <td>{{ $value->exDescripcion }}</td>
-                <td>{{ $value->exFechaInicio }}</td>
-                <td>{{ $value->fechaFinal }}</td>
-            </tr>
-            @endforeach
-        </table>
-        <h2 style="color: #636b6f;">Formaciones</h2>
-        <table class="table table-bordered">
-            <tr>
-                <th scope="col">Titulo</th>
-                <th scope="col">Especialidad</th>
-                <th scope="col">Institución</th>
-                <th scope="col">Fecha</th>
-            </tr>
-            @foreach ($formaciones as $key => $value)
-            <tr>
-                <td>{{ $value->foTitulo }}</td>
-                <td>{{ $value->foEspecialidad }}</td>
-                <td>{{ $value->foInstitucion }}</td>
-                <td>{{ $value->foFecha }}</td>
-            </tr>
-            @endforeach
-        </table>
-    </div> -->
+   
     <div class="wrapper mt-lg-5">
 
         <div class="sidebar-wrapper">
@@ -105,48 +41,6 @@
                 </ul>
             </div>
             @endforeach
-            <!--//contact-container-->
-            <div class="education-container container-block">
-                <h2 class="container-block-title">Education</h2>
-                @foreach ($formaciones as $key => $value)
-                <div class="item">
-                    <h4 class="degree">{{ $value->foTitulo }}</h4>
-                    <h5 class="degree">{{ $value->foEspecialidad }}</h5>
-
-                    <h5 class="meta">{{ $value->foInstitucion }}</h5>
-                    <div class="time">{{ $value->foFecha }}</div>
-                </div>
-                @endforeach
-
-                <!--//item-->
-                <!-- <div class="item">
-                    <h4 class="degree">BSc in Applied Mathematics</h4>
-                    <h5 class="meta">Bristol University</h5>
-                    <div class="time">2012 - 2016</div>
-                </div> -->
-                <!--//item-->
-            </div>
-            <!--//education-container-->
-
-            <div class="languages-container container-block">
-                <h2 class="container-block-title">Languages</h2>
-                <ul class="list-unstyled interests-list">
-                    <li>English <span class="lang-desc">(Native)</span></li>
-                    <li>French <span class="lang-desc">(Professional)</span></li>
-                    <li>Spanish <span class="lang-desc">(Professional)</span></li>
-                </ul>
-            </div>
-            <!--//interests-->
-
-            <!-- <div class="interests-container container-block">
-                <h2 class="container-block-title">Interests</h2>
-                <ul class="list-unstyled interests-list">
-                    <li>Climbing</li>
-                    <li>Snowboarding</li>
-                    <li>Cooking</li>
-                </ul>
-            </div> -->
-            <!--//interests-->
         </div>
         <!--//sidebar-wrapper-->
 
