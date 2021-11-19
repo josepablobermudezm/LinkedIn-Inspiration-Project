c40c14cc4823f661a8d78eff96f7d14941e07122
JocserAraya <80366138+JocserAraya@users.noreply.github.com>
2021-11-18 20:33:36 -0600

    ajustes

--- resources/views/Reportes.blade.php
+++ resources/views/Reportes.blade.php
@@ -11,36 +11,37 @@
                     {{ session('status') }}
                 </div>
                 @endif
-                <div class="container" >
-                    <div class="row">
-                        <div class="col-4">
-                            <a href="{{ route('pdf2') }}"><button id="Button2" class="Button1"
-                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
-                                        style="font-size: 20px; cursor: pointer;">Empleos</label></button></a>
-                        </div>
-                        <div class="col-4">
-                            <a href="{{ route('listaEmpresas') }}"><button id="Button3" class="Button1"
-                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
-                                        style="font-size: 20px; cursor: pointer;">Empresas</label></button></a>
-                        </div>
-
-                        <div class="col-4">
-                            <a href="{{ route('pdf5') }}"><button id="Button1" class="Button1"
-                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
-                                        style="font-size: 20px; cursor: pointer;">Gráfico</label></button></a>
-                        </div>
+                <div class="container">
+                    <div class="d-flex flex-row justify-content-around align-items-center">
                         <?php if (auth()->user()->tipoUsuario == 'C') { ?>
-                        <div class="col">
+                        <div class="">
                             <a href="{{ route('CurriculumsMenu') }}"><button id="Button3" class="Button1"
                                     onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
                                         style="font-size: 20px; cursor: pointer;">Curriculum</label></button></a>
                         </div>
-                        <div class="col">
+                        <div class="">
                             <a href="{{ route('listaOfertas') }}"><button id="Button4" class="Button1"
                                     onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
                                         style="font-size: 20px; cursor: pointer;">Ofertas</label></button></a>
                         </div>
                         <?php } ?>
+                        <div class=" ">
+                            <a href="{{ route('pdf2') }}"><button id="Button2" class="Button1"
+                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
+                                        style="font-size: 20px; cursor: pointer;">Empleos</label></button></a>
+                        </div>
+                        <div class=" ">
+                            <a href="{{ route('listaEmpresas') }}"><button id="Button3" class="Button1"
+                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
+                                        style="font-size: 20px; cursor: pointer;">Empresas</label></button></a>
+                        </div>
+
+                        <div class=" ">
+                            <a href="{{ route('pdf5') }}"><button id="Button1" class="Button1"
+                                    onclick="CambiarColor(this)"><i class="fas fa-scroll"></i><br><label
+                                        style="font-size: 20px; cursor: pointer;">Gráfico</label></button></a>
+                        </div>
+
                     </div>
                 </div>
             </div>
